762f56afcddeec7ce7f7b653023a86fed524627c
pshpark7 <pshpark07@gmail.com>
2023-12-20 11:51:15 +0900

    1단계, 종료기능 구현

--- src/wise_saying_2023_12/Main.java
+++ src/wise_saying_2023_12/Main.java
@@ -1,9 +1,14 @@
 package wise_saying_2023_12;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("123");
 
+		Scanner sc = new Scanner(System.in);
+
+		new App(sc).run();
+
+		sc.close();
 	}
-
 }
