c4b18a27dbc379a9c61656974e0fe7be97d73b5e
pshpark7 <pshpark07@gmail.com>
2023-12-20 17:37:40 +0900

    5단계,목록 명령어 실행시 모든 명언이 최신순으로 나오게

--- src/wise_saying_2023_12/App.java
+++ src/wise_saying_2023_12/App.java
@@ -17,9 +17,9 @@ public class App {
 		int lastId = 0;
 		List<WiseSaying> wiseSayings = new ArrayList<>();
 		while (true) {
-			System.out.print("명령어 ) ");
+			System.out.print("명령어  ) ");
 			String cmd = sc.nextLine().trim();
-			
+
 			if (cmd.equals("종료")) {
 				break;
 			} else if (cmd.equals("등록")) {
@@ -28,22 +28,25 @@ public class App {
 				String content = sc.nextLine().trim();
 				System.out.print("작가 : ");
 				String author = sc.nextLine().trim();
-				
+
 				WiseSaying wiseSaying = new WiseSaying(id, content, author);
 				wiseSayings.add(wiseSaying);
-				
+
 				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
 				lastId++;
-				
-			} else if(cmd.equals("목록")) {
-				if(wiseSayings.size() == 0) {
+
+			} else if (cmd.equals("목록")) {
+				if (wiseSayings.size() == 0) {
 					System.out.println("등록된 명언이 없습니다.");
 				} else {
-					System.out.println("명언이 있습니다.");
-					System.out.println("등록 된 명언 수: " + wiseSayings.size());
-					
+					System.out.println("번호 / 작가 / 명언");
+					System.out.println("==================");
+					for (int i = wiseSayings.size()-1; i >= 0;i--)  {
+						WiseSaying ws = wiseSayings.get(i);
+						System.out.printf("번호 : %d / 작가 : %s / 명언 : %s\n", ws.getId(),ws.getAuthor(),ws.getContent());
+					}
 				}
-				
+
 			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
