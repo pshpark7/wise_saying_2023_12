1de9575fbc82a9bf89cee9ffbc07cbe92abcee45
pshpark7 <pshpark07@gmail.com>
2023-12-20 16:03:15 +0900

    4단계, 명언class 생성, 등록된 명언 객체를 List에 저장

--- src/wise_saying_2023_12/App.java
+++ src/wise_saying_2023_12/App.java
@@ -1,5 +1,7 @@
 package wise_saying_2023_12;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,24 +13,40 @@ public class App {
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-		// trim() : 혹시 있을지 모르는 좌우 공백 제거
-		int n = 1;
+
+		int lastId = 0;
+		List<WiseSaying> wiseSayings = new ArrayList<>();
 		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
-
+			
 			if (cmd.equals("종료")) {
 				break;
 			} else if (cmd.equals("등록")) {
+				int id = lastId + 1;
 				System.out.print("명언 : ");
 				String content = sc.nextLine().trim();
 				System.out.print("작가 : ");
 				String author = sc.nextLine().trim();
-				System.out.printf("%d번 명언이 등록되었습니다.\n");
-				n++;
+				
+				WiseSaying wiseSaying = new WiseSaying(id, content, author);
+				wiseSayings.add(wiseSaying);
+				
+				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+				lastId++;
+				
+			} else if(cmd.equals("목록")) {
+				if(wiseSayings.size() == 0) {
+					System.out.println("등록된 명언이 없습니다.");
+				} else {
+					System.out.println("명언이 있습니다.");
+					System.out.println("등록 된 명언 수: " + wiseSayings.size());
+					
+				}
+				
+			} else {
+				System.out.println("존재하지 않는 명령어입니다.");
 			}
-
 		}
 	}
-
 }
---
+++ src/wise_saying_2023_12/WiseSaying.java
@@ -0,0 +1,38 @@
+package wise_saying_2023_12;
+
+public class WiseSaying {
+	private int id;
+	private String content;
+	private String author;
+
+	public WiseSaying(int id, String content, String author) {
+		this.id = id;
+		this.content = content;
+		this.author = author;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
